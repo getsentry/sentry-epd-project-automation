3046643c3a112a01cf5fe5b315cfbdcb1a904f97
Francesco Novy <francesco.novy@sentry.io>
2024-10-10 13:26:08 +0200

    Initial implementation of webhook

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,101 @@
 import express from 'express';
+import { syncGithubProjectForIssue } from './utils/sync-gh-project';
+import { Webhooks } from '@octokit/webhooks';
+import bodyParser from 'body-parser';
 
-const port = 3000;
+interface GithubWebhookRepository {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  has_projects: boolean;
+  owner: {
+    login: string;
+    id: number;
+  };
+}
+
+interface GithubWebhookIssue {
+  id: number;
+  node_id: string;
+  number: number;
+  state: 'open' | 'closed';
+  title: string;
+  body: string;
+}
+
+const port = parseInt(process.env.PORT || '3000', 10);
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  const webhookSecret = process.env.GH_WEBHOOK_SECRET;
+  const githubToken = process.env.GH_TOKEN;
+  const projectId = process.env.GH_PROJECT_ID;
+
+  if (!projectId || !githubToken || !webhookSecret) {
+    res
+      .status(500)
+      .send('GH_PROJECT_ID, GH_TOKEN, or GH_WEBHOOK_SECRET not set');
+    return;
+  }
+
+  const eventType = req.headers['x-github-event'];
+  const signature = req.headers['x-hub-signature-256'] as string;
+  const body = req.body;
+
+  if (!webhookSecret) {
+    res.status(500).send('GH_WEBHOOK_SECRET not set');
+    return;
+  }
+
+  const webhooks = new Webhooks({
+    secret: webhookSecret,
+  });
+
+  webhooks.verify(JSON.stringify(body), signature).then((isValid) => {
+    if (!isValid) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+  });
+
+  // This event occurs when there is activity relating to sub-issues.
+  if (eventType === 'sub_issues') {
+    console.log('Sub-Issues event received');
+
+    const payload = req.body as {
+      action:
+        | 'parent_issue_added'
+        | 'parent_issue_removed'
+        | 'sub_issue_added'
+        | 'sub_issue_removed';
+
+      parent_issue_id: number;
+      parent_issue: GithubWebhookIssue;
+      sub_issue: GithubWebhookIssue;
+      sub_issue_id: number;
+      parent_issue_repo?: GithubWebhookRepository;
+      sub_issue_repo?: GithubWebhookRepository;
+      repository?: GithubWebhookRepository;
+    };
+
+    const issueId = payload.sub_issue.node_id;
+
+    if (!issueId) {
+      res.status(400).send('sub_issue_id not found');
+      return;
+    }
+
+    syncGithubProjectForIssue(githubToken, {
+      issueId,
+      projectId,
+    }).then(({ status }) => {
+      console.log(status);
+      res.send({ success: true, status });
+    });
+  }
 });
 
 app.listen(port, () => {
---
+++ src/utils/sync-gh-project.ts
@@ -0,0 +1,454 @@
+import { graphql } from '@octokit/graphql';
+
+interface IssueWithDetails {
+  id: string;
+  title: string;
+  number: number;
+  projectItems: {
+    nodes: {
+      id: string;
+      project: {
+        id: string;
+        title: string;
+      };
+      goal: {
+        id: string;
+        name: string;
+        optionId: string;
+      } | null;
+      team: {
+        id: string;
+        name: string;
+        optionId: string;
+      } | null;
+    }[];
+  };
+  parent: IssueWithDetails | null;
+}
+
+const RepoToTeamMap: Record<string, string> = {
+  'getsentry/sentry-javascript': 'Web Frontend SDKs',
+};
+
+export async function syncGithubProjectForIssue(
+  githubToken: string,
+  {
+    projectId,
+    issueId,
+  }: {
+    projectId: string;
+    issueId: string;
+  },
+): Promise<{ status: string }> {
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${githubToken}`,
+      'GraphQL-Features': 'sub_issues',
+    },
+  });
+
+  const res = await graphqlWithAuth<{
+    node: IssueWithDetails & {
+      repository: {
+        nameWithOwner: string;
+      };
+    };
+  }>(
+    /* GraphQL */ `
+      fragment IssueWithDetails on Issue {
+        id
+        title
+        number
+
+        projectItems(first: 50) {
+          nodes {
+            id
+            project {
+              id
+              title
+            }
+            goal: fieldValueByName(name: "Goal") {
+              ... on ProjectV2ItemFieldSingleSelectValue {
+                id
+                name
+                optionId
+              }
+            }
+            team: fieldValueByName(name: "Team") {
+              ... on ProjectV2ItemFieldSingleSelectValue {
+                id
+                name
+                optionId
+              }
+            }
+          }
+        }
+      }
+
+      query getParentIssue($issueId: ID!) {
+        node(id: $issueId) {
+          ... on Issue {
+            ...IssueWithDetails
+            repository {
+              nameWithOwner
+            }
+            parent {
+              ...IssueWithDetails
+              parent {
+                ...IssueWithDetails
+                parent {
+                  ...IssueWithDetails
+                  parent {
+                    ...IssueWithDetails
+                    parent {
+                      ...IssueWithDetails
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      issueId,
+    },
+  );
+
+  // e.g. getsentry/projects
+  const issueFullRepository = res.node.repository.nameWithOwner;
+
+  const teamName = RepoToTeamMap[issueFullRepository];
+
+  const root = getRoot(res.node);
+  const issue = res.node;
+
+  if (root === issue) {
+    return { status: 'Issue is root issue, skipping...' };
+  }
+
+  const rootEpdProjectItem = root.projectItems.nodes.find(
+    (node) => node.project.id === projectId,
+  );
+
+  if (!rootEpdProjectItem) {
+    return { status: 'Root issue is not in specified project, skipping...' };
+  }
+
+  // Scenario 1: Issue is not yet in project
+  const issueProjectItem = issue.projectItems.nodes.find(
+    (node) => node.project.id === projectId,
+  );
+
+  if (!issueProjectItem) {
+    console.log(
+      `Issue with ID ${issue.id} is not in EPD Projects, adding it...`,
+    );
+    // Add it to the project
+    await addProjectToIssue(graphqlWithAuth, {
+      projectId,
+      issueId: issue.id,
+
+      goalName: root.title,
+    });
+    return { status: 'Issue added to project' };
+  }
+
+  // Scenario 2: Issue is in project - check if goal is correct
+  const goal = issueProjectItem.goal;
+
+  if (!goal || goal.name !== root.title) {
+    console.log(
+      `Issue with ID ${issue.id} is in EPD Projects but has incorrect goal, updating...`,
+    );
+    await setGoalOnProjectItem(graphqlWithAuth, {
+      projectId,
+      itemId: issueProjectItem.id,
+      goalName: root.title,
+    });
+    console.log('Project goal updated!');
+  }
+
+  const team = issueProjectItem.team;
+
+  if (teamName && (!team || team.name !== teamName)) {
+    console.log(
+      `Issue with ID ${issue.id} is in EPD Projects but has incorrect team, updating...`,
+    );
+    await setTeamOnProjectItem(graphqlWithAuth, {
+      projectId,
+      itemId: issueProjectItem.id,
+      teamName,
+    });
+    console.log('Project team updated!');
+  }
+
+  return { status: 'Issue project item is up-to-date' };
+}
+
+function getRoot(issue: IssueWithDetails): IssueWithDetails {
+  if (!issue.parent) {
+    return issue;
+  }
+  return getRoot(issue.parent);
+}
+
+async function getProjectFields(
+  graphqlWithAuth: typeof graphql,
+  { projectId }: { projectId: string },
+) {
+  const res = await graphqlWithAuth<{
+    node: {
+      id: string;
+      title: string;
+      goals: {
+        id: string;
+        name: string;
+        options: { id: string; name: string }[];
+      };
+      teams: {
+        id: string;
+        name: string;
+        options: { id: string; name: string }[];
+      };
+    } | null;
+  }>(
+    /* GraphQL */ `
+      query getProject($projectId: ID!) {
+        node(id: $projectId) {
+          ... on ProjectV2 {
+            id
+            title
+            goals: field(name: "Goal") {
+              ... on ProjectV2SingleSelectField {
+                name
+                id
+                options {
+                  id
+                  name
+                }
+              }
+            }
+            teams: field(name: "Team") {
+              ... on ProjectV2SingleSelectField {
+                name
+                id
+                options {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      projectId,
+    },
+  );
+
+  return { goals: res.node?.goals, teams: res.node?.teams };
+}
+
+async function addProjectToIssue(
+  graphqlWithAuth: typeof graphql,
+  {
+    projectId,
+    issueId,
+    goalName,
+    teamName,
+  }: {
+    projectId: string;
+    issueId: string;
+    goalName: string;
+    teamName?: string;
+  },
+) {
+  // Add project to issue
+  const res = await graphqlWithAuth<{
+    addProjectV2ItemById: {
+      item: {
+        id: string;
+        project: {
+          goals: {
+            id: string;
+            name: string;
+            options: { id: string; name: string }[];
+          };
+        };
+      };
+    };
+  }>(
+    /* GraphQL */ `
+      mutation addProjectToIssue($projectId: ID!, $issueId: ID!) {
+        addProjectV2ItemById(
+          input: { projectId: $projectId, contentId: $issueId }
+        ) {
+          item {
+            id
+            project {
+              goals: field(name: "Goal") {
+                ... on ProjectV2SingleSelectField {
+                  name
+                  id
+                  options {
+                    id
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      issueId,
+      projectId,
+    },
+  );
+
+  const itemId = res.addProjectV2ItemById.item.id;
+  const goalOptions = res.addProjectV2ItemById.item.project.goals.options;
+
+  const optionId = goalOptions.find((option) => option.name === goalName)?.id;
+  const fieldId = res.addProjectV2ItemById.item.project.goals.id;
+
+  if (!optionId || !fieldId) {
+    console.log(`Goal with name ${goalName} not found in project, skipping...`);
+    return;
+  }
+
+  // Update the "Goal" field
+  console.log(`Updating goal for issue ${issueId} to ${goalName}...`);
+  await setGoalOnProjectItem(graphqlWithAuth, {
+    projectId,
+    itemId,
+    goalName,
+  });
+
+  // Update the "Team" field, if we found a match
+  if (teamName) {
+    console.log(`Updating team for issue ${issueId} to ${teamName}...`);
+    await setTeamOnProjectItem(graphqlWithAuth, {
+      projectId,
+      itemId,
+      teamName,
+    });
+  }
+}
+
+async function setGoalOnProjectItem(
+  graphqlWithAuth: typeof graphql,
+  {
+    projectId,
+    itemId,
+    goalName,
+  }: {
+    projectId: string;
+    itemId: string;
+    goalName: string;
+  },
+) {
+  const { goals } = await getProjectFields(graphqlWithAuth, {
+    projectId,
+  });
+
+  const fieldId = goals?.id;
+  const optionId = goals?.options.find(
+    (option) => option.name === goalName,
+  )?.id;
+
+  if (!fieldId || !optionId) {
+    console.log(`Goal with name ${goalName} not found in project, skipping...`);
+    return;
+  }
+
+  await setOptionFieldOnProjectItem(graphqlWithAuth, {
+    projectId,
+    itemId,
+    fieldId,
+    optionId,
+  });
+}
+
+async function setTeamOnProjectItem(
+  graphqlWithAuth: typeof graphql,
+  {
+    projectId,
+    itemId,
+    teamName,
+  }: {
+    projectId: string;
+    itemId: string;
+    teamName: string;
+  },
+) {
+  const { teams } = await getProjectFields(graphqlWithAuth, {
+    projectId,
+  });
+
+  const fieldId = teams?.id;
+  const optionId = teams?.options.find(
+    (option) => option.name === teamName,
+  )?.id;
+
+  if (!fieldId || !optionId) {
+    console.log(`Team with name ${teamName} not found in project, skipping...`);
+    return;
+  }
+
+  await setOptionFieldOnProjectItem(graphqlWithAuth, {
+    projectId,
+    itemId,
+    fieldId,
+    optionId,
+  });
+}
+
+async function setOptionFieldOnProjectItem(
+  graphqlWithAuth: typeof graphql,
+  {
+    projectId,
+    itemId,
+    fieldId,
+    optionId,
+  }: {
+    projectId: string;
+    itemId: string;
+    fieldId: string;
+    optionId: string;
+  },
+) {
+  await graphqlWithAuth(
+    /* GraphQL */ `
+      mutation updateOptionFieldOnProjectItem(
+        $projectId: ID!
+        $fieldId: ID!
+        $itemId: ID!
+        $optionId: String!
+      ) {
+        updateProjectV2ItemFieldValue(
+          input: {
+            fieldId: $fieldId
+            itemId: $itemId
+            projectId: $projectId
+            value: { singleSelectOptionId: $optionId }
+          }
+        ) {
+          projectV2Item {
+            id
+          }
+        }
+      }
+    `,
+    {
+      fieldId,
+      projectId,
+      optionId,
+      itemId,
+    },
+  );
+}
